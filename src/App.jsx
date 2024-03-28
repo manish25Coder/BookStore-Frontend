155fc31382ac608053463a7c1fe6d5b7fa2ab56d
manish25Coder <manishsingh59766@gmail.com>
2024-03-28 16:08:36 +0530

    Added Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner'
 
 
 function App() {
   return (
     <>
     <Navbar />
+    <Banner />
     </>
   )
 }
